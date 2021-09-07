1fb42db55566b3d7ef8f21fcccde12445a525578
everycasestudy <everycasestudy@gmail.com>
2021-09-07 17:56:21 +0900

    some changes

--- index.js
+++ index.js
@@ -49,3 +49,49 @@ $(".menu-trigger").click(function () {
 $(".menu-trigger").hover(function () {
     $(this).toggleClass('hover');
 });
+
+window.contact = window.contact || {};
+
+window.contact.checkValidation = function(){
+    if(!$('input[id="name"]').val() || !$('input[id="email"]').val() || !$('input[id="subject"]').val()|| !$('textarea[id="inquiry"]').val()){
+        $('input[id=submit]').attr('disabled', 'disabled');
+        return false;
+    }
+
+    $('input[id="submit"]').removeAttr('disabled');
+    return true;
+}
+
+window.contact.send = function(){
+    var name = $('input[id="name"]').val() 
+    var email = $('input[id="email"]').val() 
+    var subject = $('textarea[id="subject"]').val()
+    var inquiry = $('textarea[id="inquiry"]').val()
+    data = {
+        name: name,
+        email: email,
+        subject: subject,
+        inquiry: inquiry,
+    }
+    window.contact.ajax(data);
+}
+
+window.contact.ajax = function(data){
+    var url = 'https://script.google.com/macros/s/AKfycbyt0wND0HMfCVf78GqmtquowOmO9L5iq5Dyv4aEyMU1_xALXFhrscizueVj1ruH8beP/exec';
+    $.ajax({
+        url: url,
+        type:'POST',
+        data: data
+    }).done(function(res){
+        if(res.response != "success") {
+            console.log(JSON.stringify(res.error));
+            alert('送信失敗'); 
+            return;
+        }
+        alert('送信完了');
+    }).fail(function(){
+        alert('送信失敗'); 
+    }).always(function(){
+        location.href="./index.html";
+    })
+}
